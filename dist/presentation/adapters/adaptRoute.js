55e7108e3e31844c6b78c2d89974d5e5f29274f4
ricardo oliveira <rictecinf@yahoo.com.br>
2021-09-27 10:23:55 -0300

    removendo pasta dist

--- dist/presentation/adapters/adaptRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.adaptRoute = void 0;
-const adaptRoute = (controller) => {
-    return controller;
-};
-exports.adaptRoute = adaptRoute;
-//# sourceMappingURL=adaptRoute.js.map
